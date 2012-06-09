389b758e0d98c120cde307d4e4df5a5bd7871844
Zach Wilt <zwilt3@gatech.edu>
2012-06-09 14:27:39 -0700

    PathData class filled out

---
+++ DroidView/src/com/example/helloworld/PathData.java
@@ -0,0 +1,50 @@
+package com.example.helloworld;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import android.graphics.Path;
+
+public class PathData implements Serializable{
+
+	private ArrayList<Float> xPoints;
+	private ArrayList<Float> yPoints;
+	
+	public PathData(){
+		this(new ArrayList<Float>(), new ArrayList<Float>());
+	}
+	
+	public PathData(ArrayList<Float> xPoints, ArrayList<Float> yPoints){
+		this.xPoints = xPoints;
+		this.yPoints = yPoints;
+	}
+	
+	public void addPoint(float x, float y){
+		xPoints.add(x);
+		yPoints.add(y);
+	}
+	
+	public void removePoint(int index){
+		xPoints.remove(index);
+		yPoints.remove(index);
+	}
+	
+	public ArrayList<Float> getXPoints(){
+		return xPoints;
+	}
+	
+	public ArrayList<Float> getYPoints(){
+		return yPoints;
+	}
+	
+	public Path constructPath(){
+		Path p = new Path();
+		if (xPoints.size() == 0){
+			return p;
+		}
+		p.setLastPoint(xPoints.get(0), yPoints.get(0));
+		for (int i = 1; i < xPoints.size(); i++){
+			p.lineTo(xPoints.get(i), yPoints.get(i));
+		}
+		return p;
+	}
+}
--- WhiteboardServer/src/FileTransferClient.java
+++ WhiteboardServer/src/FileTransferClient.java
@@ -11,6 +11,11 @@ public class FileTransferClient {
 		
 	}
 	
+	public static void main(String[] args) throws UnknownHostException, IOException{
+		FileTransferClient client = new FileTransferClient("192.168.5.212", 8000);
+		System.out.println(client.getFile("/TestFile"));
+	}
+	
 	public String getFile(String filename) throws IOException{
 		DataInputStream input = new DataInputStream(socket.getInputStream());
 		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
@@ -21,20 +26,5 @@ public class FileTransferClient {
 		String fileData = IOTools.readFully(input, fileLen);
 		return fileData;
 	}
-	
-	/*public FileTransferClient(String serverIP, int serverPort, Serializable object) throws UnknownHostException, IOException{
-		socket = new Socket(serverIP, serverPort);
-		DataInputStream input = new DataInputStream(socket.getInputStream());
-		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-		output.writeInt(filename.length());//send the length, so we know when to stop
-		output.writeBytes(filename);//send the filename to the server
-		System.out.println("I wrote: " + filename);
-		output.flush();
-		int fileLen = input.readInt();
-		String fileData = IOTools.readFully(input, fileLen);
-		System.out.println("The file I read was:\n" + fileData);
-	}*/
-	
-	
 	
 }
